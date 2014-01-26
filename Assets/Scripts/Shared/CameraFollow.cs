bfc00ac23e96764e07a5fc50fb6ce7f1f64731b7
Ben Cochrane <ben@ngon.cc>
2014-01-25 20:48:22 -0500

    Some stuff

--- Assets/Scripts/Shared/CameraFollow.cs
+++ Assets/Scripts/Shared/CameraFollow.cs
@@ -4,10 +4,11 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour {
 
 	public Transform target;
+	public float height = 5f;
 
 	void LateUpdate () {
 		if (Network.isServer) {
-			transform.position = new Vector3(target.position.x, target.position.y + 5, target.position.z);
+			transform.position = new Vector3(target.position.x, target.position.y + height, target.position.z);
 		} else {
 			transform.position = target.position;
 			transform.rotation = target.rotation;
